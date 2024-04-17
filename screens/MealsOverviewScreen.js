55c8cd9b93356f0ca06b81b19813f416bf99f3e4
Arash <arash8877@gmail.com>
2024-04-17 09:52:16 +0200

    modified CategoryScreen

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,27 +1,39 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 //import { useRoute } from "@react-navigation/native";
 import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
-const MealsOverviewScreen = ({navigation, route}) => {
-    const catId = route.params.categoryId;
-   // const route = useRoute(); alternatively can use useRoute hook for components
-   // that are not registered as screen.
-   // because route and navigation props can only be used for components that are 
-   // registered as screen.
+const MealsOverviewScreen = ({ navigation, route }) => {
+  const catId = route.params.categoryId;
+  // const route = useRoute(); alternatively can use useRoute hook for components
+  // that are not registered as screen.
+  // because route and navigation props can only be used for components that are
+  // registered as screen.
 
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  const renderMealItem = (itemData) => {
+    return <MealItem title={itemData.item.title} />;
+  };
 
   return (
     <View style={styles.container}>
-        <Text>Meals Overview Screen - {catId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
-  )
-}
+  );
+};
 
 export default MealsOverviewScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 16,
-    }
+  container: {
+    flex: 1,
+    padding: 16,
+  },
 });
