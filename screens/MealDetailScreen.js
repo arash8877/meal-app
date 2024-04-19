41148b859e061812579d31b8e2d6906cede0b4ad
Arash <arash8877@gmail.com>
2024-04-19 10:20:22 +0200

    modified MealDetailScreen

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,52 +1,60 @@
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
 import { MEALS } from "../data/dummy-data";
 import MealDetails from "../components/MealDetails";
+import Subtitle from "../components/mealDetail/Subtitle";
+import List from "../components/mealDetail/List";
 
 const MealDetailScreen = ({ navigation, route }) => {
   const mealId = route.params.mealId;
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
   return (
-    <View>
-      <Image source={{ uri: selectedMeal.imageUrl }} style={styles.imag}/>
+    <ScrollView>
+      <Image source={{ uri: selectedMeal.imageUrl }} style={styles.imag} />
       <Text style={styles.title}>{selectedMeal.title}</Text>
       <MealDetails
         duration={selectedMeal.duration}
         complexity={selectedMeal.complexity}
         affordability={selectedMeal.affordability}
       />
-      <Text style={styles.subtitle}>Ingredients</Text>
-      {selectedMeal.ingredients.map((ingredient) => (
-        <Text key={ingredient}>{ingredient}</Text>
-      ))}
-      <Text style={styles.subtitle}>Steps</Text>
-      {selectedMeal.steps.map((step) => (
-        <Text key={step}>{step}</Text>
-      ))}
-    </View>
+      <View style={styles.listOuterContainer}>
+        <View style={styles.listContainer}>
+          <Subtitle>Ingredients</Subtitle>
+          <List data={selectedMeal.ingredients} />
+          <Subtitle>Steps</Subtitle>
+          <List data={selectedMeal.steps} />
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
 export default MealDetailScreen;
 
 const styles = StyleSheet.create({
-    imag: {
-        width: '100%',
-        height: 350,
-    },
-    title: {
-        fontWeight: 'bold',
-        fontSize: 24,
-        margin: 8,
-        textAlign: 'center'
-    },
-    subtitle: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        marginVertical: 4,
-        marginHorizontal: 24,
-        padding: 6,
-        textAlign: 'center',
-        borderBottomWidth: 2,
-    }
+  imag: {
+    width: "100%",
+    height: 350,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 24,
+    margin: 8,
+    textAlign: "center",
+  },
+  subtitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginVertical: 4,
+    marginHorizontal: 24,
+    padding: 6,
+    textAlign: "center",
+    borderBottomWidth: 2,
+  },
+  listOuterContainer: {
+    alignItems: "center",
+  },
+  listContainer: {
+    maxWidth: "80%",
+  },
 });
