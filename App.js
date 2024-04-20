c4c7cce7717d7950ef7f8e7522a19faff556d85a
Arash <arash8877@gmail.com>
2024-04-20 18:12:26 +0200

    added a drawer

--- App.js
+++ App.js
@@ -3,10 +3,20 @@ import { StyleSheet, Button } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function drawerNavigator() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Categories" component={CategoriesScreen} />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -21,21 +31,13 @@ export default function App() {
           }}
         >
           <Stack.Screen
-            name="MealsCategories"
-            component={CategoriesScreen}
+            name="Drawer"
+            component={drawerNavigator}
             options={{
               title: "All Categories",
             }}
           />
-          <Stack.Screen
-            name="MealsOverview"
-            component={MealsOverviewScreen}
-            //options={({route, navigation}) => {
-            //  const catId = route.params.categoryId;
-            //  return {
-            //    title: catId,
-            //  }}}
-          />
+          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
           <Stack.Screen name="MealDetail" component={MealDetailScreen} />
         </Stack.Navigator>
       </NavigationContainer>
